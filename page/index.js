475afbc4a75f31ae8d4781215c1499924f4a746c
reyhankelas7bputrabukyayah <reyhanmohamad0212@gmail.com>
2022-03-02 14:37:20 +0700

    added contact & index page

---
+++ page/index.js
@@ -0,0 +1,8 @@
+/**Untuk membuka navbar */
+function openNav(){
+    document.getElementById("myNav").style.height = "100%";
+ }
+/**Untuk menutup navbar */
+function closeNav() {
+    document.getElementById("myNav").style.display = "none";
+  }
